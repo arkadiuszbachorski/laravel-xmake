ea9eb75325eebcb514ea53f1d3bc313bad616082
Arkadiusz Bachorski <quez2223@gmail.com>
2020-02-04 15:11:11 +0100

    Refactor ExtendedGeneratorCommand to use Fields class

--- src/Commands/ExtendedGeneratorCommand.php
+++ src/Commands/ExtendedGeneratorCommand.php
@@ -8,10 +8,9 @@ use Illuminate\Support\Arr;
 
 
 class ExtendedGeneratorCommand extends GeneratorCommand {
+    use Prefix;
 
-    protected $parsedOptionFields = [];
-
-    protected $fields = [];
+    protected $fields = null;
 
     protected $type = "";
 
@@ -39,78 +38,12 @@ class ExtendedGeneratorCommand extends GeneratorCommand {
         return $this->getStubDir();
     }
 
-
-    protected function includeFieldsConfigFile()
+    protected function getFields()
     {
-        $path = base_path(config('xmake.paths.fields').'/fields.php');
-
-        if (!file_exists($path)) {
-            throw new \Error("Couldn't find fields file. You should check your config. Expected path: $path");
+        if ($this->fields === null) {
+            $this->fields = new Fields($this->option('fields'));
         }
 
-        $this->fields = include $path;
-    }
-
-    protected function parseFieldsOption()
-    {
-        $array = [];
-        foreach (explode(",", $this->option('fields')) as $item) {
-            array_push($array, $item);
-        }
-
-        $this->parsedOptionFields = $array;
-    }
-
-    protected function getFieldsDataIfEmpty() {
-        if (count($this->parsedOptionFields) === 0) {
-            $this->parseFieldsOption();
-        }
-        if (count($this->fields) === 0) {
-            $this->includeFieldsConfigFile();
-        }
-    }
-
-    protected function getDefaultFieldsWhenElementNotFound(string $name) {
-        return [
-            'name' => $name,
-            'database' => '',
-            'validation' => '',
-            'factory' => '',
-            'filled' => false,
-        ];
-    }
-
-    protected function getElementFromFields(string $name)
-    {
-        $this->getFieldsDataIfEmpty();
-
-        $element = Arr::first($this->fields, function ($value, $key) use ($name) {
-            return $key === $name;
-        });
-
-        if (!$element) {
-            $element = $this->getDefaultFieldsWhenElementNotFound($name);
-        } else {
-            $element['filled'] = true;
-        }
-
-        return $element;
-    }
-
-    protected function prefix(string $text, int $tabs = 1, bool $lineBreak = true)
-    {
-        $prefix = "";
-
-        for ($i = 0; $i < $tabs; $i++) {
-            $prefix = "    $prefix";
-        }
-
-        $prefixed = "$prefix$text";
-
-        if ($lineBreak) {
-            $prefixed = "\r\n$prefixed";
-        }
-
-        return $prefixed;
+        return $this->fields;
     }
 }
